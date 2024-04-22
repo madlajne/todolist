f61ff40be0d24ea28d2a1923f95f196c837d1e28
Magdalena Makuch <makuchmada02@wp.pl>
2024-04-22 11:53:02 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+{
+    const tasks = [
+        {
+            content: "wysłać e-mail",
+            done: true,
+        },
+        {
+            content: "posprzątać",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
