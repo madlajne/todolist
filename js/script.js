b40d859332065a2a8ef5c2a8973f41d3720404a3
Magdalena Makuch <makuchmada02@wp.pl>
2024-07-05 13:31:04 +0200

    changes toggleTaskDone function

--- js/script.js
+++ js/script.js
@@ -25,7 +25,12 @@
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            { ...tasks[taskIndex], done: !tasks[taskIndex].done },
+            ...tasks.slice(taskIndex + 1),
+        ];
+
         render();
     };
 
